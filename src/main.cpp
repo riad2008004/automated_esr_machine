3becb2cfd1555cfc9879a812c090e9a2b3357ff6
riad2008004 <arifulislamriad5@gmail.com>
2026-04-21 02:39:13 +0600

    sensor and indicator

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,11 @@ float temperature = 30.3;
 float humidity = 75.5;
 float esr[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
+// Sensor from 1 : A1,A6,A8,A5,A7,A3,A9,A4,A10,A2
+// LED From 1: 23,22,21,20,19,18,17,16,15,14
+int esr_sensor[10] = {A1, A6, A8, A5, A7, A3, A9, A4, A10, A2};
+int indicator[10] = {23, 22, 21, 20, 19, 18, 17, 16, 15, 14};
+
 // Task: Read DHT11
 void readDHTTask(void *parameter)
 {
@@ -48,7 +53,33 @@ void printTask(void *parameter)
     }
     Serial.println();
 
-    vTaskDelay(3000 / portTICK_PERIOD_MS);
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+  }
+}
+
+// Task: Monitor all sensors
+void esrTask(void *parameter)
+{
+  while (true)
+  {
+
+    for (int i = 0; i < 10; i++)
+    {
+      int sensorValue = digitalRead(esr_sensor[i]);
+
+      // Assuming LOW = test tube detected
+      if (sensorValue == LOW)
+      {
+        digitalWrite(indicator[i], HIGH);
+        vTaskDelay(pdMS_TO_TICKS(300));
+        digitalWrite(indicator[i], LOW);
+      }
+      else
+      {
+        digitalWrite(indicator[i], LOW); // LED OFF
+      }
+    }
+    vTaskDelay(pdMS_TO_TICKS(300)); // small delay for stability
   }
 }
 
@@ -56,29 +87,19 @@ void setup()
 {
   Serial.begin(9600);
   dht.begin();
+  // Initialize pins
+  for (int i = 0; i < 10; i++)
+  {
+    pinMode(esr_sensor[i], INPUT);
+    pinMode(indicator[i], OUTPUT);
+  }
 
   xTaskCreate(readDHTTask, "ReadDHT", 2048, NULL, 1, NULL);
   xTaskCreate(printTask, "Print", 2048, NULL, 1, NULL);
+  xTaskCreate(esrTask, "ESR Task", 256, NULL, 1, NULL);
 }
 
 void loop()
 {
-
-  Serial.print("TEMP=");
-  Serial.print(temperature, 1);
-
-  Serial.print(" & HUM=");
-  Serial.print(humidity, 1);
-
-  for (int i = 0; i < 10; i++)
-  {
-    Serial.print(" & ESR");
-    Serial.print(i + 1);
-    Serial.print("=");
-    Serial.print(esr[i], 1);
-  }
-
-  Serial.println();
-
-  delay(1000); // send every 1 second
+  // Running by RTOS
 }

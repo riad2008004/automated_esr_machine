8dcb93e38ee58dc657c1817939b11f603a609f9f
riad2008004 <arifulislamriad5@gmail.com>
2026-04-21 02:10:32 +0600

    added dht and passed data

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,64 @@
 #include <Arduino.h>
+#include <DHT.h>
+#include <Arduino_FreeRTOS.h>
+
+#define DHTPIN A0
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
 
 float temperature = 30.3;
 float humidity = 75.5;
-float esr[10] = {5, 5, 5, 5, 5, 5, 5, 5, 5, 5};
+float esr[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+// Task: Read DHT11
+void readDHTTask(void *parameter)
+{
+  while (true)
+  {
+    float t = dht.readTemperature();
+    float h = dht.readHumidity();
+
+    if (!isnan(t) && !isnan(h))
+    {
+      temperature = t;
+      humidity = h;
+    }
+
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+  }
+}
+
+// Task 2: Print Data
+void printTask(void *parameter)
+{
+  while (true)
+  {
+    Serial.print("TEMP=");
+    Serial.print(temperature, 1);
+
+    Serial.print(" & HUM=");
+    Serial.print(humidity, 1);
+
+    for (int i = 0; i < 10; i++)
+    {
+      Serial.print(" & ESR");
+      Serial.print(i + 1);
+      Serial.print("=");
+      Serial.print(esr[i], 1);
+    }
+    Serial.println();
+
+    vTaskDelay(3000 / portTICK_PERIOD_MS);
+  }
+}
 
 void setup()
 {
   Serial.begin(9600);
+  dht.begin();
+
+  xTaskCreate(readDHTTask, "ReadDHT", 2048, NULL, 1, NULL);
+  xTaskCreate(printTask, "Print", 2048, NULL, 1, NULL);
 }
 
 void loop()
@@ -26,7 +78,7 @@ void loop()
     Serial.print(esr[i], 1);
   }
 
-  Serial.println(); // end line
+  Serial.println();
 
   delay(1000); // send every 1 second
 }
